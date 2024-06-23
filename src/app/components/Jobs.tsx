6ca8085c019b5ba036d2958f0857f4de2ec71e27
nishk1409 <nishthak1409@gmail.com>
2024-06-23 16:12:46 +0530

    day 2 basic front end code for landing page completed

---
+++ src/app/components/Jobs.tsx
@@ -0,0 +1,18 @@
+export default function Jobs (){
+    return (
+        <div className="bg-slate-200 p-8 rounded-xl ">
+        <div className="Container">
+        <h2 className=" font-bold mb-4">Recent jobs</h2>
+        </div>
+
+        <div className="flex flex-col gap-4">
+        <div className="bg-white p-6 rounded-md shadow-md">test job</div>
+        <div className="bg-white p-6 rounded-md shadow-md">test job</div>
+        <div className="bg-white p-6 rounded-md shadow-md">test job</div>
+        <div className="bg-white p-6 rounded-md shadow-md">test job</div>
+        </div>
+
+        </div>
+    )
+
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,23 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    container: {
+      screens: {
+          'lg': '1024px',
+          'xl': '1024px',
+          '2xl': '1024px'
+          },
+          padding: {
+            DEFAULT: '1rem',
+          }
       },
-    },
+      extend: {
+        backgroundImage: {
+          "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+          "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        },
+      },
   },
   plugins: [],
 };
